2001644bd5af52eb3ed6445075355137e7fe5757
ksm987 <40618438+KimSM98@users.noreply.github.com>
2020-06-05 23:07:14 +0900

    0605
    Update: 색상별 공격 이펙트 구현
    Needs: 눌렀던 버튼이 공격시 꺼질 것

--- Media Project2020-1/Assets/Scripts/ColorButton.cs
+++ Media Project2020-1/Assets/Scripts/ColorButton.cs
@@ -11,16 +11,16 @@ public class ColorButton : MonoBehaviour
 
     public void GetButtonColor()
     {
-        if(UIManagerScript.instance.PushedButtonNum <2 && isPushed != true){//최대 2개 누를 수 있고, 선택된 것은 작동 x
+        if(AttackManagerScript.instance.PushedButtonNum <2 && isPushed != true){//최대 2개 누를 수 있고, 선택된 것은 작동 x
             isPushed = true;
-            UIManagerScript.instance.PushColorButton(ButtonColorNum, ArrayNum);
+            AttackManagerScript.instance.PushColorButton(ButtonColorNum, ArrayNum);
             
         }//다시 누르면 뗌
-        else if(UIManagerScript.instance.PushedButtonNum>0 && isPushed == true){
+        else if(AttackManagerScript.instance.PushedButtonNum>0 && isPushed == true){
             isPushed = false;
-            UIManagerScript.instance.releaseColorButton(ButtonColorNum,ArrayNum);
+            AttackManagerScript.instance.releaseColorButton(ButtonColorNum,ArrayNum);
         }
-        UIManagerScript.instance.ChangeMSBSprite();
+        AttackManagerScript.instance.ChangeMSBSprite();
     }
     
 }
